37aa5f6c5dc96dfe3e71689579961f35d893ab24
mads150 <botvinnikova90@mail.ru>
2018-12-27 23:35:42 +1000

    Подготовка проекта к защите

--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,3 @@
-
 var button = document.querySelector(".forwarding-button");
 
 var popup = document.querySelector(".modal-feedback");
@@ -36,25 +35,25 @@ close.addEventListener("click", function(evt) {
   popup.classList.remove("modal-error");
 });
 
-form.addEventListener("submit", function (evt) {
-   if (!namefield.value || !email.value) {
-     evt.preventDefault();
-     popup.classList.remove("modal-error");
-     popup.offsetWidth = popup.offsetWidth;
-     popup.classList.add("modal-error");
-   } else {
-     if (isStorageSupport) {
-       localStorage.setItem("namefield", namefield.value);
-     }
+form.addEventListener("submit", function(evt) {
+  if (!namefield.value || !email.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("namefield", namefield.value);
+    }
   }
- });
+});
 
- window.addEventListener("keydown", function(evt) {
-   if (evt.keyCode === 27) {
-     if (popup.classList.contains("modal-show")) {
-       evt.preventDefault();
-       popup.classList.remove("modal-show");
-       popup.classList.remove("modal-error");
-     }
-   }
- });
+window.addEventListener("keydown", function(evt) {
+  if (evt.keyCode === 27) {
+    if (popup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
+    }
+  }
+});
